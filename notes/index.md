1249d05bc0251001e43a5e5eb4c791a1a0652586
Nic Ollis <nic@oll.is>
2019-03-18 11:43:14 -0400

    Add notes

---
+++ notes/index.md
@@ -0,0 +1,4 @@
+# Notes
+Welcome to my notes directory. You might have mistyped a shortlink, if so it drop you in here. Feel free to look for the notes you where looking for below.
+
+- [Notes from Meetup Talks](/notes/meetups)
--- redirect.py
+++ redirect.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 
 urls = {
-  'indyrb': 'https://gist.github.com/nicollis/972ae12c823b853a01b5f57ef961f4f7',
+  'indyrb': '/notes/meetups',
   'default': 'https://nic.oll.is'
 }
 
